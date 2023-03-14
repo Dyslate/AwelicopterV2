eabf7cd4f3d0f9e732f05f9cc53a7b1412651913
dys <dys@DESKTOP-THR4JM3>
2023-03-14 15:38:31 +0100

    First push d'un truck ok-tier

---
+++ src/awele/bot/competitor/Awelicopter/Awelicopter.java
@@ -0,0 +1,41 @@
+package awele.bot.competitor.Awelicopter;
+
+import awele.bot.CompetitorBot;
+import awele.core.Board;
+import awele.core.InvalidBotException;
+
+import java.util.Arrays;
+
+
+public class Awelicopter extends CompetitorBot {
+
+    public Awelicopter () throws InvalidBotException
+    {
+        this.setBotName ("Awelicopter");
+        this.addAuthor ("Lucas FRANCHINA");
+        this.addAuthor ("Tim BRANSTETT");
+
+    }
+
+
+    @Override
+    public void initialize()  {
+    }
+
+    @Override
+    public void finish() {
+
+    }
+
+    @Override
+    public double[] getDecision(Board board) {
+        MinMaxNodeAwelicopter.initialize (board, 4);
+        return new MaxNodeAwelicopter(board).getDecision ();
+    }
+
+
+
+    @Override
+    public void learn() {
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,14 @@
+module ia_project {
+
+	exports awele.core;
+	exports awele.data;
+	exports awele.bot;
+	exports awele.run;
+	exports awele.bot.demo.random;
+	exports awele.output;
+
+
+	requires java.desktop;
+	requires javassist;
+	requires reflections;
+}
